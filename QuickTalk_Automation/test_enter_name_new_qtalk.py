46042701af958d9cc57039a06f55453b96df9f92
Pushpander Shukla <pushpander@buzz4health.com>
2022-03-11 11:35:54 +0530

    changed sleep to 5 in testclick created card

--- QuickTalk_Automation/test_enter_name_new_qtalk.py
+++ QuickTalk_Automation/test_enter_name_new_qtalk.py
@@ -6,7 +6,7 @@ def testenter_name_for_qtalk():
     funtion.open_browser()
     funtion.continue_with_Google_button()
     funtion.Click_Qtalk()
-    funtion.driver.find_element(By.ID, "formBasicEmail").send_keys("jfhdfhsdbfhsdfbghsd")
+    funtion.driver.find_element(By.ID, "formBasicEmail").send_keys("dfbsadfbvsdfuyh")
     time.sleep(10)
     funtion.click_create_quicktalk_button()
     funtion.home()
@@ -16,10 +16,10 @@ def testClick_on_the_created_card():
     card_elements = funtion.driver.find_elements(By.CLASS_NAME, "card-main ")
     for card_elem in card_elements:
         title_elem = card_elem.find_element(By.CLASS_NAME, "talk--title")
-        if title_elem.text == "jfhdfhsdbfhsdfbghsd":
+        if title_elem.text == "dfbsadfbvsdfuyh":
             print(title_elem.text)
             funtion.driver.execute_script("arguments[0].scrollIntoView()", card_elem)
             print(card_elem)
             time.sleep(3)
             card_elem.click()
-            time.sleep(10)
+            time.sleep(5)
