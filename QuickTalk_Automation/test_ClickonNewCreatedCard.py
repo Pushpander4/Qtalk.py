c9f7abbda8898ef69affb72b4f6a9fe44b17bb57
Piyush Wadhwa <piyush.wadhwa.703@gmail.com>
2022-03-11 22:19:09 +0530

    changed classnames

--- QuickTalk_Automation/test_ClickonNewCreatedCard.py
+++ QuickTalk_Automation/test_ClickonNewCreatedCard.py
@@ -1,12 +1,14 @@
 import test_enter_name_new_qtalk
 import funtion
+from selenium.webdriver.common.by import By
+import time
 
 
 def testclickon_new_created_card():
-    card_elements = funtion.driver.find_elements(By.CLASS_NAME, "home-feed")
+    card_elements = funtion.driver.find_elements(By.CLASS_NAME, "card-main ")
     print(card_elements)
     for card_elem in card_elements:
-        title_elem = card_elem.find_element(By.CLASS_NAME, "card--title")
+        title_elem = card_elem.find_element(By.CLASS_NAME, "talk--title")
         if title_elem.text == "Automate quickTalk Created":
             print(title_elem.text)
             funtion.driver.execute_script("arguments[0].scrollIntoView()",card_elem)
