1bae62591db01670e036ac2fe6c6f0caba87f6f7
Piyush Wadhwa <piyush.wadhwa.703@gmail.com>
2022-03-13 00:28:41 +0530

    added chrome_options=chrome_options

--- QuickTalk_Automation/funtion.py
+++ QuickTalk_Automation/funtion.py
@@ -9,7 +9,7 @@ chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
 parent_handel = driver.current_window_handle
 
 
