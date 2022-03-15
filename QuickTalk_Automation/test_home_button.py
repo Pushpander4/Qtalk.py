8ebe907118ac9e23a9e6e263a595c4109a014c87
Piyush Wadhwa <piyush.wadhwa.703@gmail.com>
2022-03-15 12:03:06 +0530

    Improved test for home button

--- QuickTalk_Automation/test_home_button.py
+++ QuickTalk_Automation/test_home_button.py
@@ -1,9 +1,15 @@
 import time
 
+from selenium.webdriver.common.by import By
+
 import funtion
 
 funtion.open_browser()
 
 
-def testhome_button():
-    funtion.home()
+def test_home_button():
+    funtion.driver.find_element(By.XPATH, "//a[normalize-space()='Home']").is_enabled()
+    print("Home button Enable=", funtion.home)
+    HomeText = funtion.driver.find_element(By.XPATH, "//a[normalize-space()='Home']").text
+    assert (HomeText == "Home")
+    funtion.driver.find_element(By.XPATH, "//a[normalize-space()='Home']").click()
